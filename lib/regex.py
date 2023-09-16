36800a5af78451a4dbf573cca3c4d0d6b616e307
steve-the-hermit <stevemacharia309@gmail.com>
2023-09-16 09:54:23 +0300

    done

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,83 @@
 import re
 
-my_pattern = r""
-my_regex = re.compile(my_pattern)
+def fullmatch(pattern, string, flags=0):
+    """
+    Match the entire string against the pattern and return a match object if successful.
+    """
+    return re.fullmatch(pattern, string, flags)
 
+def findall(pattern, string, flags=0):
+    """
+    Find all non-overlapping matches of the pattern in the string and return them as a list of strings.
+    """
+    return re.findall(pattern, string, flags)
+
+import re
+
+def fullmatch(pattern, string, flags=0):
+    """
+    Match the entire string against the pattern and return a match object if successful.
+    """
+    return re.fullmatch(pattern, string, flags)
+
+def findall(pattern, string, flags=0):
+    """
+    Find all non-overlapping matches of the pattern in the string and return them as a list of strings.
+    """
+    return re.findall(pattern, string, flags)
+
+import re
+
+def fullmatch(pattern, string, flags=0):
+    """
+    Match the entire string against the pattern and return a match object if successful.
+    """
+    return re.fullmatch(pattern, string, flags)
+
+def findall(pattern, string, flags=0):
+    """
+    Find all non-overlapping matches of the pattern in the string and return them as a list of strings.
+    """
+    return re.findall(pattern, string, flags)
+
+import re
+
+def fullmatch(pattern, string, flags=0):
+    """
+    Match the entire string against the pattern and return a match object if successful.
+    """
+    return re.fullmatch(pattern, string, flags)
+
+def findall(pattern, string, flags=0):
+    """
+    Find all non-overlapping matches of the pattern in the string and return them as a list of strings.
+    """
+    return re.findall(pattern, string, flags)
+
+import re
+
+def fullmatch(pattern, string, flags=0):
+    """
+    Match the entire string against the pattern and return a match object if successful.
+    """
+    return re.fullmatch(pattern, string, flags)
+
+def findall(pattern, string, flags=0):
+    """
+    Find all non-overlapping matches of the pattern in the string and return them as a list of strings.
+    """
+    return re.findall(pattern, string, flags)
+
+import re
+
+def fullmatch(pattern, string, flags=0):
+    """
+    Match the entire string against the pattern and return a match object if successful.
+    """
+    return re.fullmatch(pattern, string, flags)
+
+def findall(pattern, string, flags=0):
+    """
+    Find all non-overlapping matches of the pattern in the string and return them as a list of strings.
+    """
+    return re.findall(pattern, string, flags)
